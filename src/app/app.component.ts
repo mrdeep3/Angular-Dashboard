fba240fe180f9f3b8e4f952d3ea7b01636be715e
Deep B Chitroda <43362100+mrdeep3@users.noreply.github.com>
2020-01-26 23:36:41 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // 2 decorators required at the minimum
+  templateUrl: './app.component.html', // can take only a single string
+  styleUrls: ['./app.component.css'] // can contain an array of strings
 })
 export class AppComponent {
-  title = 'angular-example';
+  title = 'angular-app';
 }
