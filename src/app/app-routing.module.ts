564412855e5a6a012c35cf92cac929a307c84d55
Deep B Chitroda <43362100+mrdeep3@users.noreply.github.com>
2020-01-26 23:34:36 -0500

    added 2 new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'}, //edirecting the empty path to the home component
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'equipment', component: EquipmentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
