2ab5e68a42221a5eecef58079b4f74c3e097cc9a
Deep B Chitroda <43362100+mrdeep3@users.noreply.github.com>
2020-01-30 21:47:09 -0500

    included pagination for table

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  config: any;
+  collection = { count: 9, data: [] }; // 9 values in table
+ 
+  constructor() {
+    function myfunc()
+    {
+      var loc_list=['Milling','Packaging','Processing','Extracting'];
+      var loc = loc_list[Math.floor(Math.random()*loc_list.length)];
+      return loc;
+    } 
+    //hardcoded data
+    var loc_list=['Milling','Packaging','Processing','Extracting'];
+    for (var i = 0; i < this.collection.count; i++) {
+      this.collection.data.push(
+        {
+          id: i + 1,
+          name: "ABC " + (i + 1),
+          type:"Type " + (i+1),
+          performance: i,
+          efficiency: i+1,
+          location: myfunc()
+        }
+      );
+    }
+    this.config = {
+      itemsPerPage: 3,
+      currentPage: 1,
+      totalItems: this.collection.count
+    };
   }
 
+  pageChanged(event){
+    this.config.currentPage = event;
+  }
+  ngOnInit() {
+  }
 }
